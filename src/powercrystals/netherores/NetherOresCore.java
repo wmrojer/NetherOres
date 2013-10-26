f8e51b8d4ef4655dcfae5480be8e3e4cf35ca1ba
skyboy <skyboy026@gmail.com>
2013-10-26 01:59:30 -0400

    Mark beta

--- src/powercrystals/netherores/NetherOresCore.java
+++ src/powercrystals/netherores/NetherOresCore.java
@@ -47,7 +47,7 @@ connectionHandler = ConnectionHandler.class)
 public class NetherOresCore extends BaseMod
 {
 	public static final String modId = "NetherOres";
-	public static final String version = "1.6.2R2.2.0";
+	public static final String version = "1.6.2R2.2.1B1";
 	public static final String modName = "Nether Ores";
 	
 	public static final String mobTextureFolder = "netherores:textures/mob/";
