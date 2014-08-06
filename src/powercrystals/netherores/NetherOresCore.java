afd32aeceefbcf83d41cdb142b0f30deb511c02e
skyboy <skyboy026@gmail.com>
2014-08-06 01:26:21 -0400

    Promote to RC3

--- src/powercrystals/netherores/NetherOresCore.java
+++ src/powercrystals/netherores/NetherOresCore.java
@@ -55,7 +55,7 @@ public class NetherOresCore extends BaseMod
 {
 	public static final String modId = "NetherOres";
 	public static final String modName = "Nether Ores";
-	public static final String version = "1.7.10R2.3.0RC2";
+	public static final String version = "1.7.10R2.3.0RC3";
 	public static final String dependencies = CoFHProps.DEPENDENCIES +
 			";required-after:CoFHCore@[" + CoFHProps.VERSION + ",)";
 
